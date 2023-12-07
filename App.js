a76701228fbd8e30b6b889d2326d47de9ffe7a74
trahongquan <lovenevertolate97@gmail.com>
2023-12-07 20:26:13 +0700

    Bài tập flex

--- App.js
+++ App.js
@@ -3,9 +3,23 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container} >
+      <View style={styles.Top}>
+        <Text style={styles.text}>Top</Text>
+      </View>
+      <View style={styles.Bottom}>
+        <View style={styles.BottomLeft}>
+          <Text>Bottom left</Text>
+        </View>
+        <View style={styles.BottomRight}>
+          <View style={styles.RightBottomTop}>
+            <Text>Right Bottom Top</Text>
+          </View>
+          <View style={styles.RightBottomBottom}>
+            <Text>Right Bottom Bottom</Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,7 +28,47 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+  text:{
+    // color: 'black',
+    // alignSelf: 'center',
+    // textAlign: 'center'
+  },
+  Top:{
+    flex:1,
+    backgroundColor: '#F3FE7D',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  Bottom:{
+    flex:1,
+    backgroundColor: '#37EB7A',
+    flexDirection: 'row',
+  },
+  BottomLeft:{
+    flex:1,
+    backgroundColor: '#37EB7A',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  BottomRight:{
+    flex:1,
+    backgroundColor: 'white',
+  },
+  RightBottomTop:{
+    flex:2,
+    backgroundColor: '#EA35E2',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  RightBottomBottom:{
+    flex:1,
+    backgroundColor: '#2776AA',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+
 });
