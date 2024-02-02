f12764d43ac5ad18eaf9f46b4927d146be36f5fd
trahongquan <lovenevertolate97@gmail.com>
2024-02-02 09:40:35 +0700

    update ImputTask

--- components/Form/InputTask.js
+++ components/Form/InputTask.js
@@ -1,8 +1,49 @@
-import { View, TextInput, Image, KeyboardAvoidingView, Platform, TouchableOpacity } from 'react-native'
+import { View, TextInput, Image, KeyboardAvoidingView, Platform, TouchableOpacity } from 'react-native';
 import styles from "./StyleInputTask";
-import React from 'react'
+import React from 'react';
+import Dialog from "react-native-popup-dialog";
+// import * as Notifications from 'expo-notifications';
+
+// async function getNotificationToken(){
+//   const {status} =  await Notifications.getPermissionsAsynsc()
+//   if(status !== 'granted'){
+//       const {status} =  await Notifications.requestgetPermissionsAsynsc()
+//       if(status !== 'granted'){
+//         alert('failed')
+//         return
+//       }
+//   }
+//   const tokenData = await Notifications.getPermissionsAsynsc()
+//   const token = tokenData.data
+//   return token
+// }
+
+// Notifications.setNotificationHandler({
+//   handleNotification: async () ({
+//   shouldShowAlert: true, 
+//   shouldPlaySound: false, 
+//   shouldSetBadge: false, 
+//   }), 
+// }),
 
 const InputTask = () => {
+
+  const [isDialogVisible, setDialogVisible] = React.useState(false);
+
+  const showDialog = () => {
+    setDialogVisible(true);
+  };
+
+  const handlePress = () => {
+    // Hiển thị dialog khi bấm vào BottomItemImg
+    Alert.alert("Thông báo", "Bạn đã bấm vào BottomItemImg!");
+    showDialog();
+  };
+
+  const handleDialogClose = () => {
+    // Đóng dialog khi người dùng nhấn vào nút đóng hoặc bên ngoài dialog
+    setDialogVisible(false);
+  };
   return (
     <KeyboardAvoidingView
     keyboardVerticalOffset={10}
@@ -13,7 +54,7 @@ const InputTask = () => {
                 <TextInput style={styles.TextInput}
                 placeholder="Aa"/>
             </View>
-            <TouchableOpacity>
+            <TouchableOpacity onPress={handlePress} >
               <View style={styles.BottomItemImg}>
                   <Image style={styles.Img} source={require('./../../assets/add-icon.png')} />
               </View>
